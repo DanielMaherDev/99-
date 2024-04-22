704f16b4ddfe1d0ec0114d81c8370f45ab181d56
DanielMaherDev <dmdanielmaher@gmail.com>
2024-04-22 23:22:00 +0000

    initial commit, added HTML and JS skeleton @

---
+++ main.js
@@ -0,0 +1,25 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    // Load your assets here
+}
+
+function create() {
+    // Create game objects here
+}
+
+function update() {
+    // Game logic goes here
+}
